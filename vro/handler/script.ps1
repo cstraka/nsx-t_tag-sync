db483dcdf35e1922996cb9ba2b9c81ea08d06a6f
Craig Straka <craig.straka@it-partners.com>
2021-01-22 15:25:39 -0700

    Updated for VRO version of the script

--- vro/handler/script.ps1
+++ vro/handler/script.ps1
@@ -1,12 +1,5 @@
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationWarnings $false -ParticipateInCeip $false -Confirm:$false | Out-Null
-
 # Process function Secrets passed in
-
-#production inputs
 $SECRETS_FILE = "/var/openfaas/secrets/vro-secrets"
-#Testing Secrets Input Location
-$SECRETS_FILE = "D:\OneDrive\GitHub\NSX-T_Tag-Sync\vro\vro-secrets.json"
-
 $SECRETS_CONFIG = (Get-Content -Raw -Path $SECRETS_FILE | ConvertFrom-Json)
 
 # Process payload sent from vCenter Server Event
@@ -15,20 +8,26 @@ if($env:function_debug -eq "true") {
     Write-Host "DEBUG: json=`"$($json | Format-List | Out-String)`""
 }
 
-$vcenter = ($json.source -replace "https://","" -replace "/sdk","");
-$vmMoRef = $json.data.vm.vm.value;
-$vm = $json.data.vm.name;
+$vro = $SECRETS_CONFIG.VRO_SERVER;
+$vroUser = $SECRETS_CONFIG.VRO_USERNAME;
+$vroWorkflowID = $SECRETS_CONFIG.VRO_WORKFLOW_ID;
+$vcenter = ($json.source -replace "https://","" -replace "/sdk","")
 
-$vcenter = "vcenter7-phx.itplab.local"
-$vmMoRef = "vm-1086"
-$vm = "blue-app202"
+$separator = "object"," "
+$option = [System.StringSplitOptions]::RemoveEmptyEntries
+$FullFormattedMessage = $json.data.FullFormattedMessage.split($separator,$option)
+$FullFormattedMessage = $FullFormattedMessage.split([Environment]::NewLine)
 
-#Assigning credentials securely
-$credentials = $host.ui.PromptForCredential("Input Your Virtual Center credentials", "Please enter your vCenter user name and password.", "", "NetBiosUserName")
+$vm = $FullFormattedMessage[$FullFormattedMessage.count-1]
 
-#connecting to VI server
-Connect-VIServer -Server $vcenter -Protocol https -Credential $credentials
-
+if($env:function_debug -eq "true") {
+    Write-Host "DEBUG: VRO=$vro"
+    Write-Host "DEBUG: vm=$vroUser"
+    Write-Host "DEBUG: vCenter=$vcenter"
+    Write-Host "DEBUG: vmMoRef=$vmMoRef"
+    Write-Host "DEBUG: vm=$vm"
+    Write-Host "DEBUG: workflowID=$vroWorkflowID"
+}
 if($vmMoRef -eq "" -or $vm -eq "") {
     Write-Host "Unable to retrieve VM Object from Event payload, please ensure Event contains VM result"
     exit
@@ -37,60 +36,48 @@ if($vmMoRef -eq "" -or $vm -eq "") {
 # e.g. mgmt-vcsa-01.cpbu.corp/vm-2660
 $vroVmId = "$vcenter/$vmMoRef"
 
-$jsonTags = @{}
-$vmList = New-Object System.Collections.ArrayList
-$tagList = New-Object System.Collections.ArrayList
-$tags = Get-VM -name $vm -server $vcenter | Get-TagAssignment
-$vmID = Get-VM -name $vm -server $vcenter
-foreach ($tag in $tags)
+$body = @"
 {
-    $tagString = $tag.tag.ToString()
-    $tagArray = $tagString.split('/')
-    $tagList.add(@{"category"=$tagArray[0];"value"=$tagArray[1]})
+    "parameters":
+	[
+        {
+            "value": {
+                "sdk-object":{
+                    "type": "VC:VirtualMachine",
+                    "id": "$($vroVmId)"}
+                },
+            "type": "VC:VirtualMachine",
+            "name": "$vm",
+            "scope": "local"
+        }
+	]
 }
-$vmList.add(@{"viServer"=$vcenter;"name"=$vm;"vmPersisitentID"=$vmID.PersistentID;"vmID"=$vmID.Id;"tags"=$tagList;})
-$jsonTags.add("data",$vmList)
-
-$jsonTags | ConvertTo-Json -depth 10 | Out-File "d:\virtualmachines.json"
-$jsonBody = $jsonTags | ConvertTo-Json -depth 10
-#write-host $jsonBody
-$body = $jsonBody #| ConvertFrom-Json
-Write-Host $body
-#Write-Host "Disconnecting from vCenter Server ..."
-Disconnect-VIServer * -Confirm:$false
+"@
 
 # Basic Auth for vRO execution
-#$pair = "svc_vco@itplab.local:Passw0rd!"
 $pair = "$($SECRETS_CONFIG.VRO_USERNAME):$($SECRETS_CONFIG.VRO_PASSWORD)"
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
 $base64 = [System.Convert]::ToBase64String($bytes)
 $basicAuthValue = "Basic $base64"
 
-$vroAuthURL = "https://$($SECRETS_CONFIG.VRO_SERVER):443/vco/api/workflows"
-$headers = @{
-    "Authorization"="$basicAuthValue";
-    "Accept="="application/json";
-    "Content-Type"="application/json";
-}
-$authResponse = Invoke-WebRequest -uri $vroAuthURL -Method GET -Headers $headers -SkipHeaderValidation -SkipCertificateCheck
-
-$vroUrl = "https://$($SECRETS_CONFIG.VRO_SERVER):443/vco/api/workflows/$($SECRETS_CONFIG.VRO_WORKFLOW_ID)/executions"
 $headers = @{
     "Authorization"="$basicAuthValue";
     "Accept="="application/json";
     "Content-Type"="application/json";
 }
 
-$env:function_debug = 'true'
+$vroUrl = "https://$($vro):443/vco/api/workflows/$($SECRETS_CONFIG.VRO_WORKFLOW_ID)/executions"
+
 if($env:function_debug -eq "true") {
     Write-Host "DEBUG: vRoVmID=$vroVmId"
+    Write-Host "DEBUG: TagCategory=$($SECRETS_CONFIG.TAG_CATEGORY_NAME)"
+    Write-Host "DEBUG: TagName=$($SECRETS_CONFIG.TAG_NAME)"
     Write-Host "DEBUG: vRoURL=`"$($vroUrl | Format-List | Out-String)`""
     Write-Host "DEBUG: headers=`"$($headers | Format-List | Out-String)`""
     Write-Host "DEBUG: body=$body"
 }
 
-$env:skip_vro_cert_check = 'true'
-
+Write-Host "Synchronizing vSphere Tags to VM: $vm in NSX-T"
 if($env:skip_vro_cert_check -eq "true") {
     Invoke-Webrequest -Uri $vroUrl -Method POST -Headers $headers -SkipHeaderValidation -Body $body -SkipCertificateCheck
 } else {
