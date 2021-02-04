bb28385338203fe06f44bfbfb94a7a3598ee41a8
Craig Straka <craig.straka@it-partners.com>
2021-02-03 19:20:41 -0700

    updating testing for event content

--- handler/script.ps1
+++ handler/script.ps1
@@ -39,8 +39,8 @@ Set-PowerCLIConfiguration -InvalidCertificateAction Ignore  -DisplayDeprecationW
 $json = $args | ConvertFrom-Json
 
 if($env:function_debug -eq "true") {
-    Write-Host "DEBUG: json=`"$($args | Format-List | Out-String)`"" {
-        foreach ($argument in $args.Arguments) {
+    Write-Host "DEBUG: json=`"$($json | Format-List | Out-String)`"" {
+        foreach ($argument in $json.Arguments) {
             Write-Host 'key='$argument.key
             write-host 'value='$argument.Value
         }
